6f882753a24659cc155d8254a8e683b1a3edd6d6
gui4458 <gui4458@gmail.com>
2024-02-23 14:39:18 +0900

    main

---
+++ src/main/java/com/green/Nolloo/NollooApplication.java
@@ -0,0 +1,13 @@
+package com.green.Nolloo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NollooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NollooApplication.class, args);
+	}
+
+}
